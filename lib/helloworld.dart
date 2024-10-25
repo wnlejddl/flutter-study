7f8b409cf2bbffe89a3a5faa34d396e5b0830afb
wnlejddl <stopovermoon@gmail.com>
2024-10-25 22:07:48 +0900

    ch01 - add multiple line

--- lib/helloworld.dart
+++ lib/helloworld.dart
@@ -4,21 +4,25 @@ class HelloWorld extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
-      title: 'Hello World Travel Title',
+      title: 'Hello World',
       home: Scaffold(
         appBar: AppBar(
           title: Text('App Bar'),
           backgroundColor: Colors.deepPurple,
         ),
         body: Center(
-          child: Text(
-            'Hello',
-            style: TextStyle(
-                fontSize: 26,
-                fontWeight: FontWeight.bold,
-                color: Colors.blue[800]),
-          ),
-        ),
+            child: Column(
+          children: [
+            Text('first line'),
+            Text(
+              'second line',
+              style: TextStyle(
+                  color: Colors.amber[400],
+                  fontWeight: FontWeight.bold,
+                  fontSize: 28),
+            )
+          ],
+        )),
       ),
     );
   }
